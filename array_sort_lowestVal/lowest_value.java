256142386a1fd0e900143a1a9f7b4601f80926d0
uriah2006 <uriah2006@gmail.com>
2013-10-30 18:40:56 -0400

    working. added some comments and cleaned up code

--- array_sort_lowestVal/lowest_value.java
+++ array_sort_lowestVal/lowest_value.java
@@ -1,41 +1,54 @@
 package array_sort_lowestVal;
 
+import java.util.Random;
+
 public class lowest_value {
 
 	public static void main(String[] args) {
-		int i;
-		int[] array = new int[10];
 
+		int i; // Iterator
+		int[] array = new int[10];// creates in array of size 10
+		
+		// random generator (btw i like this)
+		final Random r = new Random(System.currentTimeMillis());
+
+		// fills the array
+		for (i = 0; i < array.length; i++) {
+			array[i] = r.nextInt(1000) - 500;
+		}
+
+		// prints array with descriptions
 		System.out.print("your array is: {");
 		for (i = 0; i < array.length; i++) {
-			
-			array[i] = (int) ((Math.random() * 1000) - 500);
 			if (!(i == array.length - 1)) {
 				System.out.print(array[i] + ",");
 			} else {
-				System.out.println(array[i]+"}");
+				System.out.println(array[i] + "}");
 			}
 		}
-		System.out.println("the lowest value in the array is:"+lowest(array));
+
+		// prints out lowest value in array
+		System.out.println("the lowest value in the array is:" + lowest(array));
 	}
 
 	private static int lowest(int[] array) {
-		array = sort(array);
-		return( (array[0] < array[array.length-1]) ? array[0] : array[array.length-1]);
-		
+		array = bubbleSort(array);
+		return ((array[0] < array[array.length - 1]) ? array[0]
+				: array[array.length - 1]);
+
 	}
 
 	// yeah i know its is slow but it is not what i am focused on
-	private static int[] sort(int[] array) {
+	private static int[] bubbleSort(int[] array) {
 		int sorted = 1;
 		int temp;
 		while (!(sorted == 0)) {
 			sorted = 0;
 			for (int i = 0; i < array.length - 1; i++) {
-				if (array[i] > array[i+1]) {
+				if (array[i] > array[i + 1]) {
 					temp = array[i];
-					array[i] = array[i+1];
-					array[i+1] = temp;
+					array[i] = array[i + 1];
+					array[i + 1] = temp;
 					sorted++;
 				}
 			}
